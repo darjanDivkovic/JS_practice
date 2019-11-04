58456966f06aa9309a3e0008d6c9b1b1fa44f4aa
Darjan Divkovic <daarjan.tz@gmail.com>
2019-11-04 13:05:26 +0100

    mhm

--- calculator/calcScript.js
+++ calculator/calcScript.js
@@ -1,1 +1,7 @@
 // selecting variables
+let screen = document.querySelector(".calc-text");
+let num1 = document.querySelector(".calc-child1");
+
+// Add event listener
+num1.addEventListener("click", changeScreenNumAdded(this.text));
+
