025a817d8965beb3f329e0c79d062c6a2c6f0ed7
Darjan Divkovic <daarjan.tz@gmail.com>
2019-11-12 18:11:58 +0100

    mhm, finally

--- Promisses/promise.js
+++ Promisses/promise.js
@@ -1,1 +1,12 @@
-alert("Maksuz selam!");
+let promise = new Promise((resolve, reject) => {
+    let answer = prompt("Ko je najjaci ?", "Coda il Daco");
+
+    if(answer === "Coda"){
+        resolve("Bravo!");
+    }
+    else reject("Nije tacno");
+});
+
+promise.finally( () => {
+    alert("Dobro si i reko");
+});
